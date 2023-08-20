e6a56c443e5148e03abe4da75745c0fdf7a2089d
Rajnish <rajnish6633@gmail.com>
2023-08-20 16:31:03 +0530

    feat: swagger UI documentation added

--- app.js
+++ app.js
@@ -2,10 +2,11 @@ require("dotenv").config();
 require("express-async-errors");
 
 const express = require("express");
+
 const app = express();
 const fileUpload = require("express-fileupload");
 
-//all other packages
+// all other packages
 const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
 const rateLimiter = require("express-rate-limit");
@@ -14,17 +15,23 @@ const xss = require("xss-clean");
 const cors = require("cors");
 const mongoSanitize = require("express-mongo-sanitize");
 
-//database
+// Swager;
+const swaggerUI = require("swagger-ui-express");
+const YAML = require("yamljs");
+
+const swaggerDocument = YAML.load("./swagger.yaml");
+
+// database
 const connectDB = require("./db/connect");
 
-//routes
+// routes
 const authRouter = require("./routes/authRoutes");
 const userRouter = require("./routes/userRoutes");
 const productRouter = require("./routes/productRoutes");
 const reviewRouter = require("./routes/reviewRoutes");
 const orderRouter = require("./routes/orderRoutes");
 
-//error handeling
+// error handeling
 const errorHandlerMiddleware = require("./middlewares/error-handler");
 const notFoundMiddleware = require("./middlewares/not-found");
 
@@ -41,7 +48,7 @@ app.use(cors());
 app.use(xss());
 app.use(mongoSanitize());
 
-//extra packages
+// extra packages
 app.use(express.json());
 app.use(cookieParser(process.env.JWT_SECRET));
 app.use(morgan("tiny"));
@@ -49,6 +56,11 @@ app.use(morgan("tiny"));
 app.use(express.static("./public"));
 app.use(fileUpload());
 
+app.get("/", (req, resp) => {
+  resp.send('<h1>E-Commerce API</h1><a href="/api-docs">Documentation</a>');
+});
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
+// routes
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/products", productRouter);
--- controllers/orderController.js
+++ controllers/orderController.js
@@ -1,61 +1,59 @@
-const { StatusCodes } = require( "http-status-codes" );
-const Order = require( "../models/OrderModel" );
-const Product = require( "../models/ProductModel" );
+const { StatusCodes } = require("http-status-codes");
+const Order = require("../models/OrderModel");
+const Product = require("../models/ProductModel");
 
-const CustomError = require( "../errors" );
-const checkPermissions = require( "../utils/checkPermissions" );
+const CustomError = require("../errors");
+const checkPermissions = require("../utils/checkPermissions");
 
-const fakeStripeAPI = async ( { amount, currency } ) => {
+const fakeStripeAPI = async ({ amount, currency }) => {
   const client_secret = "someRandomValue";
   return { client_secret };
 };
 
-const getAllOrders = async ( req, resp ) => {
-  const orders = await Order.find( {} );
+const getAllOrders = async (req, resp) => {
+  const orders = await Order.find({});
 
-  resp.status( StatusCodes.OK ).json( { orders } );
+  resp.status(StatusCodes.OK).json({ orders });
 };
 
-const getSingleOrder = async ( req, resp ) => {
+const getSingleOrder = async (req, resp) => {
   const { id: orderId } = req.params;
-  const order = await Order.findOne( { _id: orderId } );
+  const order = await Order.findOne({ _id: orderId });
 
-  if ( !order ) {
-    throw new CustomError.NotFoundError( `No Order with id ${orderId}` );
+  if (!order) {
+    throw new CustomError.NotFoundError(`No Order with id ${orderId}`);
   }
-  checkPermissions( req.user, order.user );
-  resp.status( StatusCodes.OK ).json( { order } );
+  checkPermissions(req.user, order.user);
+  resp.status(StatusCodes.OK).json({ order });
 };
 
-const getCurrentUserOrders = async ( req, resp ) => {
-  const orders = await Order.find( { user: req.user.userId } );
+const getCurrentUserOrders = async (req, resp) => {
+  const orders = await Order.find({ user: req.user.userId });
 
-  resp.status( StatusCodes.OK ).json( { orders } );
+  resp.status(StatusCodes.OK).json({ orders });
 };
 
-const createOrder = async ( req, resp ) => {
+const createOrder = async (req, resp) => {
   const { items: cartItems, tax, shippingFee } = req.body;
-  if ( !cartItems || cartItems.length < 1 ) {
-    throw new CustomError.BadRequestError( "No Cart items provided" );
+  if (!cartItems || cartItems.length < 1) {
+    throw new CustomError.BadRequestError("No Cart items provided");
   }
-  if ( !tax || !shippingFee ) {
+  if (!tax || !shippingFee) {
     throw new CustomError.BadRequestError(
-      "Please provide tax and shipping fee",
+      "Please provide tax and shipping fee"
     );
   }
 
   let orderItems = [];
   let subtotal = 0;
 
-  for ( const item of cartItems ) {
-    const dbProduct = await Product.findOne( { _id: item.product } );
+  for (const item of cartItems) {
+    const dbProduct = await Product.findOne({ _id: item.product });
 
-    if ( !dbProduct ) {
-      throw new CustomError.NotFoundError( `No product with id ${item.product}` );
+    if (!dbProduct) {
+      throw new CustomError.NotFoundError(`No product with id ${item.product}`);
     }
-    const {
-      name, price, image, _id,
-    } = dbProduct;
+    const { name, price, image, _id } = dbProduct;
     const singleOrderItem = {
       amount: item.amount,
       name,
@@ -64,19 +62,19 @@ const createOrder = async ( req, resp ) => {
       product: _id,
     };
 
-    orderItems = [ ...orderItems, singleOrderItem ];
+    orderItems = [...orderItems, singleOrderItem];
     subtotal += price * item.amount;
   }
   // calculate total
   const total = tax + shippingFee + subtotal;
 
   // get client secret
-  const paymentIntent = await fakeStripeAPI( {
+  const paymentIntent = await fakeStripeAPI({
     amount: total,
     currency: "in",
-  } );
+  });
 
-  const order = await Order.create( {
+  const order = await Order.create({
     orderItems,
     total,
     subtotal,
@@ -84,29 +82,29 @@ const createOrder = async ( req, resp ) => {
     shippingFee,
     clientSecret: paymentIntent.client_secret,
     user: req.user.userId,
-  } );
+  });
 
   resp
-    .status( StatusCodes.CREATED )
-    .json( { order, clientSecret: order.clientSecret } );
+    .status(StatusCodes.CREATED)
+    .json({ order, clientSecret: order.clientSecret });
 };
 
-const updateOrder = async ( req, resp ) => {
+const updateOrder = async (req, resp) => {
   const { id: orderId } = req.params;
   const { paymentIntentId } = req.body;
 
-  const order = await Order.findOne( { _id: orderId } );
-  if ( !order ) {
-    throw new CustomError.NotFoundError( `No Order with id ${orderId}` );
+  const order = await Order.findOne({ _id: orderId });
+  if (!order) {
+    throw new CustomError.NotFoundError(`No Order with id ${orderId}`);
   }
 
-  checkPermissions( req.user, order.user );
+  checkPermissions(req.user, order.user);
 
   order.paymentIntentId = paymentIntentId;
   order.status = "paid";
   await order.save();
 
-  resp.status( StatusCodes.OK ).json( { order } );
+  resp.status(StatusCodes.OK).json({ order });
 };
 
 module.exports = {
