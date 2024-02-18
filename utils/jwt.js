1489f1e8300f3b6bf41c719fce4a07338bf0eb81
Rajnish Kumar <rajnish6633@gmail.com>
2024-02-18 21:27:04 +0530

    fix : cookie fixes

--- utils/jwt.js
+++ utils/jwt.js
@@ -13,12 +13,27 @@ const isTokenValid = ({ token }) => jwt.verify(token, process.env.JWT_SECRET);
 const attachCookiesToResponse = ({ res, user }) => {
   token = createJWT({ payload: user });
   const oneDay = 1000 * 60 * 60 * 24;
-  res.cookie("token", token, {
-    httpOnly: true,
-    expires: new Date(Date.now() + oneDay),
-    secure: process.env.NODE_ENV === "production",
-    signed: true,
-  });
+
+  let cookieConfig = {};
+  if (process.env.NODE_ENV === "production") {
+    cookieConfig = {
+      httpOnly: true,
+      expires: new Date(Date.now() + oneDay),
+      secure: true,
+      signed: true,
+      SameSite: "None",
+    };
+  } else {
+    cookieConfig = {
+      httpOnly: true,
+      domain: "localhost",
+      expires: new Date(Date.now() + oneDay),
+      secure: false,
+      sameSite: "lax",
+      signed: true,
+    };
+  }
+  res.cookie("token", token, cookieConfig);
 };
 module.exports = {
   createJWT,
