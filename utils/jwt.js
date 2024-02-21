af897cd840ccb79d7f94aa3960264811fe5c0f78
Rajnish Kumar <rajnish6633@gmail.com>
2024-02-22 00:13:38 +0530

    cookie fixes

--- utils/jwt.js
+++ utils/jwt.js
@@ -15,33 +15,26 @@ const attachCookiesToResponse = ({ res, user }) => {
   const oneDay = 1000 * 60 * 60 * 24;
 
   let cookieConfig = {};
-  // if (process.env.NODE_ENV === "production") {
-  //   cookieConfig = {
-  //     httpOnly: true,
-  //     expires: new Date(Date.now() + oneDay),
-  //     secure: true,
-  //     signed: true,
-  //     SameSite: "None",
-  //   };
-  // } else {
-  //   cookieConfig = {
-  //     httpOnly: true,
-  //     domain: "localhost",
-  //     expires: new Date(Date.now() + oneDay),
-  //     secure: false,
-  //     sameSite: "lax",
-  //     signed: true,
-  //   };
-  // }
+  if (process.env.NODE_ENV === "production") {
+    cookieConfig = {
+      httpOnly: true,
+      expires: new Date(Date.now() + oneDay),
+      signed: true,
+      secure: true,
+      sameSite: "None",
+      domain: "e-commerce-api-yyqb.onrender.com",
+    };
+  } else {
+    cookieConfig = {
+      httpOnly: true,
+      domain: "localhost",
+      expires: new Date(Date.now() + oneDay),
+      secure: false,
+      sameSite: "lax",
+      signed: true,
+    };
+  }
 
-  cookieConfig = {
-    httpOnly: true,
-    expires: new Date(Date.now() + oneDay),
-    signed: true,
-    secure: true,
-    sameSite: "None",
-    domain: "e-commerce-api-yyqb.onrender.com",
-  };
   res.cookie("token", token, cookieConfig);
 };
 module.exports = {
